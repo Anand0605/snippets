8a1db1cdc33bd43572dc241f271bfed24b03f1b2
Anand Kumar Gautam <anandvns0510@gmail.com>
2025-05-27 01:30:54 +0530

    code snippet complete

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import { Button } from "@/components/ui/button";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import * as actions from "@/actions"
 
 // Props for dynamic route
 type SnippetDetailsProps = {
@@ -10,6 +11,8 @@ type SnippetDetailsProps = {
 
 const SnippetDetailPage = async ({ params }: SnippetDetailsProps) => {
   const id = parseInt(params.id);
+
+  await new Promise((r)=>setTimeout(r,2000))
 
   if (isNaN(id)) {
     throw new Error("Invalid snippet ID");
@@ -23,6 +26,8 @@ const SnippetDetailPage = async ({ params }: SnippetDetailsProps) => {
     return notFound();
   }
 
+
+  const deleteSnippetActions = actions.deleteSnippet.bind(null,snippet.id)
   return (
     <section className="max-w-3xl mx-auto mt-10 p-6 bg-white rounded-2xl shadow-md border">
       {/* Header */}
@@ -32,7 +37,7 @@ const SnippetDetailPage = async ({ params }: SnippetDetailsProps) => {
           <Link href={`/snippets/${snippet.id}/edit`}>
             <Button>Edit</Button>
           </Link>
-          <Button variant="destructive">Delete</Button>
+          <form action={deleteSnippetActions} ><Button variant="destructive" type="submit">Delete</Button></form>
         </div>
       </div>
 
--- src/components/EditSnippetForm.tsx
+++ src/components/EditSnippetForm.tsx
@@ -3,20 +3,36 @@
 import { Editor } from "@monaco-editor/react";
 import React, { useState } from "react";
 import type { Snippet } from "@prisma/client";
+import { saveSnippet } from "@/actions";
+import { Button } from "./ui/button";
 
 const EditSnippetForm = ({ snippet }: { snippet: Snippet }) => {
   const [code, setCode] = useState(snippet.code);
 
-  return (
-    <div>
-      <Editor
-        height="90vh"
-        theme="vs-dark"
-        defaultLanguage="javascript"
-        defaultValue={code}
-      />
-    </div>
-  );
+
+  const changeEventHandler = (value: string = "") => {
+  setCode(value);
+};
+
+  const saveSnippetAction = saveSnippet.bind(null,snippet.id,code)
+
+ return (
+  <div className="flex flex-col gap-4">
+    <form action={saveSnippetAction} className="flex items-center justify-between">
+      <h1 className="font-bold text-xl">Your Code Editor:</h1>
+      <Button type="submit">Save</Button>
+    </form>
+
+    <Editor
+      height="40vh"
+      theme="vs-dark"
+      defaultLanguage="javascript"
+      defaultValue={code}
+      onChange={changeEventHandler}
+    />
+  </div>
+);
+
 };
 
 export default EditSnippetForm;
